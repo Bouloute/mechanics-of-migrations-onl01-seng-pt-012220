d71e458855bcdb93961bdff30fb1705405dd092e
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-03-30 16:13:51 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -4,7 +4,7 @@ class CreateArtists < ActiveRecord::Migration[5.2]
       t.string :name
       t.string :genre
       t.integer :age
-      t.string :hometown
+      t.string :homebob
     end
   end
 
